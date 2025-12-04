861cfa4e133664b073a9218054cc6b427bfcadc0
ivo-shorgov <ivo.shorgov@gmail.com>
2025-12-04 19:03:51 +0200

    format car_travel.c with some new lines

--- adv-tasks/car_travel.c
+++ adv-tasks/car_travel.c
@@ -4,6 +4,7 @@ int main()
     int h1, m1;
     int h2, m2;
     int travel_times_in_mins;
+
     printf("start hour(h1): ");
     scanf("%d", &h1); 
     printf("start minute(m1): ");
@@ -12,7 +13,10 @@ int main()
     scanf("%d", &h2);
     printf("end minute(m2): ");
     scanf("%d", &m2);
+
     travel_times_in_mins = (h2 * 60 + m2) - (h1 * 60 + m1);
+
     printf("The car was traveling for %d hours and %d minutes\n", travel_times_in_mins / 60, travel_times_in_mins % 60);
+    
     return 0;
 }
