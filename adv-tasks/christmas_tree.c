2a9db6dcaeb3ae08fb287d88627ca6caf571845a
ivo-shorgov <ivo.shorgov@gmail.com>
2025-12-15 18:20:48 +0200

    remade cristmas tree

--- adv-tasks/christmas_tree.c
+++ adv-tasks/christmas_tree.c
@@ -1,28 +1,46 @@
-#include<stdio.h>
-int main() {
-    int n, i, j;
+#include <stdio.h>
+int main()
+{
+    //inputs the hight of the Christmas tree
+    int n;
     printf("Enter the height of the Christmas tree: ");
     scanf("%d", &n);
 
+    //validates the input
+    while (n < 1)
+    {
+        printf("invalid integer for n: \n ");
+        printf("n = ");
+        scanf("%d", &n);
+    }
 
-    for(i = 1; i <= n; i++) {
+    //defines the height of the base and crown of the Christmas tree
+    int base_height = 1;
+    int crown_height = n - base_height;
 
-        for(j = i; j < n; j++) {
+    //prints the upper part of the Christmas tree
+    for (int i = 0; i < crown_height; i++)
+    {
+        for (int k = 0; k < crown_height - i - 1; k++)
+        {
             printf(" ");
         }
-
-        for(j = 1; j <= (2 * i - 1); j++) {
-            printf("*");
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("* ");
         }
         printf("\n");
     }
 
- 
-    for(i = 1; i <= n / 3; i++) {
-        for(j = 1; j < n; j++) {
+    //prints the base of the Christmas tree
+    for (int m = 0; m < base_height ; m++)
+    {
+        for (int l = 0; l < crown_height - 2; l++)
+        {
             printf(" ");
         }
-        printf("||\n");
+        printf("| |");
+        printf("\n");
     }
 
     return 0;
